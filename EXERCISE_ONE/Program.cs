5caf684971b1499d8a00582b9b0c349bd62a0fe9
Ekaterina Shvalyuk <ekaterinashvalyuk@AirdiEkaterina.lan>
2022-10-07 18:39:03 +0200

    ex2

--- EXERCISE_ONE/Program.cs
+++ EXERCISE_ONE/Program.cs
@@ -1,2 +1,29 @@
-﻿
-Console.WriteLine("Hello, World2!");
+﻿//homework ex 2
+//a=5;b=7->max=7
+//a=2;b=10->max=10
+//a=-9;b=-3->max=-3
+Console.WriteLine("Enter two numbers to compare");
+Console.WriteLine("Введи первое число:");
+int a = 
+Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число:");
+int b=
+Convert.ToInt32(Console.ReadLine());
+
+bool num1 = a > b;
+bool num2 = a == b;
+bool num3 = a < b;
+
+if (num1)
+{
+    Console.WriteLine($"number {a} bigger than {b}");
+}
+else if (num2)
+{
+    Console.WriteLine($"Number {a} and {b} equal");
+}
+else if (num3)
+{
+    Console.WriteLine($"Number {a} smaller that {b}");
+}
+
